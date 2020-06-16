79a0c3b5953239cef8db5a829c94d019aa63268f
István Bozsó <bozsoistvan93@gmail.com>
2020-06-16 12:06:29 +0200

    added iterator to be used with jet templates

--- doc/doc.go
+++ doc/doc.go
@@ -2,6 +2,7 @@ package doc
 
 import (
     "time"
+    "reflect"
     "github.com/bozso/gotoolbox/path"
 )
 
@@ -30,6 +31,51 @@ func (d Doc) DateWithFmt(layout string) (s string) {
     return d.date.Format(layout)
 }
 
-func (d Doc) NewPath(args ...string) (p path.Path) {
+func (_ Doc) NewPath(args ...string) (p path.Path) {
     return path.Joined(args...)
+}
+
+type Indices struct {
+    start, stop, step int
+}
+
+func (in Indices) Start(start int) (ind Indices) {
+    in.start = start
+    return in
+}
+
+type Iter struct {
+    Indices
+    current int 
+}
+
+func (in Indices) Iter() (it Iter) {
+    it.Indices, it.current = in, it.start
+    return
+}
+
+func (it *Iter) Range() (reflect.Value, reflect.Value, bool) {
+    for it.current < it.stop {
+        curr := reflect.ValueOf(it.current)
+        it.current += it.step
+        
+        return curr, curr, false 
+    }
+    
+    return reflect.Value{}, reflect.Value{}, true
+}
+
+func (in Indices) Step(step int) (ind Indices) {
+    in.step = step
+    return in
+}
+
+func (in Indices) Stop(stop int) (ind Indices) {
+    in.stop = stop
+    return in
+}
+
+func (_ Doc) Indices(stop int) (ind Indices) {
+    ind.stop, ind.start, ind.step = stop, 0, 1
+    return
 }
