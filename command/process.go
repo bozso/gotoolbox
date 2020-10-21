7aa7107874110ee1afc8a2c069ac324504d9fb53
István Bozsó <bozso.istvan@csfk.mta.hu>
2020-10-21 23:24:07 +0200

    Added documentation.

--- command/process.go
+++ command/process.go
@@ -4,6 +4,10 @@ import (
     "os/exec"
 )
 
+/*
+A simple wrapper around a command, handling an executable binary to be
+started and used as process and handled by another struct.
+*/
 type Process struct {
     process *exec.Cmd
 }
@@ -14,6 +18,11 @@ func NewProcess(exec *exec.Cmd) (c Command) {
     }
 }
 
+
+/*
+A wrapper around a process that pipes input to the started process,
+handles output and errors returned by the process.
+*/
 type StartedProcess struct {
     Process
     Stderr io.ReadCloser
@@ -21,6 +30,9 @@ type StartedProcess struct {
     Stdin  io.WriteCloser
 }
 
+/*
+Start a process and setup it's pipes.
+ */
 func (p Process) Start() (s StartedProcess, err error) {
     if s.Stderr, err = p.process.StderrPipe(); err != nil {
         return
@@ -42,6 +54,10 @@ func (p Process) Start() (s StartedProcess, err error) {
     return
 }
 
+/* 
+Send data to the started process. After writing to the stdin pipe,
+the function reads from the stderr and reports any errors.
+ */
 func (s *StartedProcess) Write(b []byte) (n int, err error) {
     if n, err = s.Stdin.Write(b); err != nil {
         return
@@ -58,21 +74,28 @@ func (s *StartedProcess) Write(b []byte) (n int, err error) {
     }
 }
 
+// Waits for the process to close.
 func (s StartedProcess) Wait() (err error) {
     return s.process.Wait()
 }
 
+/*
+An error describing the context of a failed communication with a
+process.
+ */
 type Error struct {
     Exec string,
     Message []byte,
     err error
 }
 
+// Implements the error interface.
 func (e Error) Error() (s string) {
     return fmt.Sprintf("process '%s' returned with error: '%s'",
         e.Exec, e.Message)
 }
 
+// Can be unwrapped.
 func (e Error) Unwrap() (err error) {
     return e.err
 }
