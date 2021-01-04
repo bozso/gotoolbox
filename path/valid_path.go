cced79645abee15737a14b9a8dffa1e21ba0d14e
István Bozsó <bozso.istvan@csfk.mta.hu>
2021-01-04 13:10:15 +0100

    formatting

--- path/valid_path.go
+++ path/valid_path.go
@@ -33,7 +33,7 @@ func (v Valid) ToFile() (vf ValidFile, err error) {
     if err != nil {
         return
     }
-    
+
     if isDir {
         err = fmt.Errorf("path '%s' is not a file, but a directory", v)
     }
@@ -44,11 +44,11 @@ func (v Valid) ToFile() (vf ValidFile, err error) {
 
 func (vp Valid) Stat() (fi os.FileInfo, err error) {
     fi, err = os.Lstat(vp.String())
-    
+
     if err != nil {
         err = vp.Fail(OpStat, err)
     }
-    
+
     return
 }
 
@@ -57,9 +57,9 @@ func (vp Valid) ModTime() (t time.Time, err error) {
     if err != nil {
         return
     }
-    
+
     t = fi.ModTime()
-    return 
+    return
 }
 
 func (v Valid) OlderThan(vp Valid) (b bool, err error) {
@@ -72,7 +72,7 @@ func (v Valid) OlderThan(vp Valid) (b bool, err error) {
     if err != nil {
         return
     }
-    
+
     b = t1.After(t2)
     return
 }
@@ -89,19 +89,19 @@ func (vp Valid) IsDir() (b bool, err error) {
     if err != nil {
         return
     }
-    
+
     b = fi.IsDir()
     return
 }
 
 func (vp Valid) Open() (of *os.File, err error) {
     of, err = os.Open(vp.String())
-    
+
     if err != nil {
         err = vp.Fail(OpOpen, err)
     }
-    
-    return 
+
+    return
 }
 
 func (vp Valid) Remove() (err error) {
@@ -110,12 +110,12 @@ func (vp Valid) Remove() (err error) {
 
 func (vp Valid) Rename(target Pather) (dst Valid, err error) {
     s1, s2 := vp.String(), target.AsPath().String()
-    
+
     if err = os.Rename(s1, s2); err != nil {
         err = errors.WrapFmt(err, "failed to move '%s' to '%s'", s1, s2)
         return
     }
-    
+
     dst, err = New(s2).ToValid()
     return
 }
@@ -125,9 +125,8 @@ func (vp Valid) Move(dir Dir) (dst Valid, err error) {
     if err != nil {
         return
     }
-    
+
     dst, err = vp.Rename(_dst)
-    
     return
 }
 
@@ -139,7 +138,7 @@ const (
 
 func (op validPathOperation) Fmt(p Path) (s string) {
     ps := p.GetPath()
-    
+
     switch op {
     case OpOpen:
         s = fmt.Sprintf("failed to open path '%s'", ps)
