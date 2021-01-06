b8467ab4a26154a8c1213c5ca207564cbee4cfec
István Bozsó <bozso.istvan@csfk.mta.hu>
2021-01-06 17:17:37 +0100

    tweaked documentation

--- path/use.go
+++ path/use.go
@@ -6,16 +6,17 @@ import (
 )
 
 /*
-ReadCreator represents an object that can create a `ReadCloser`.
+ReadCreator represents an object that can create a ReadCloser.
 */
 type ReaderCreator interface {
     CreateReader() (r io.ReadCloser, err error)
 }
 
 /*
-UseReader creates a `ReadCloser` object, applies the `fn` function to it
-(uses the resource), and finally closes it. This ensures the 
-of such 
+UseReader creates a ReadCloser object, applies the fn` function to it
+(uses the resource), and finally closes it. This ensures that the error
+reported by the Close method is returned and not ignored and clearly
+denotes the lifetime of a ReadCloser instance.
 */
 func UseReader(r ReaderCreator, fn func (io.ReadCloser) error) (err error) {
     rc, err := r.CreateReader()
@@ -30,8 +31,8 @@ func UseReader(r ReaderCreator, fn func (io.ReadCloser) error) (err error) {
 }
 
 /*
-UseAsScanner creates a `ReaderCloser` and wraps it inside a `bufio.Scanner`
-struct to be used by `fn`.
+UseAsScanner creates a ReaderCloser and wraps it inside a bufio.Scanner
+struct to be used by fn.
 */
 func UseAsScanner(r ReaderCreator, fn func(*bufio.Scanner) error) (err error) {
     rc, err := r.CreateReader()
