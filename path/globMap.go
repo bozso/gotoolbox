2e2b257793171fbf61c217510257836f40e37a5c
István Bozsó <bozso.istvan@csfk.mta.hu>
2021-01-06 14:16:51 +0100

    added GlobPattern map

---
+++ path/globMap.go
@@ -0,0 +1,27 @@
+package path
+
+import (
+    "github.com/bozso/gotoolbox/errors"
+
+)
+
+type GlobPatterns map[string]GlobPattern
+
+func (g GlobPatterns) Get(key string, ext Extension) (f []ValidFile, err error) {
+    p, ok := g[key]
+    if !ok {
+        err = errors.KeyNotFound(key)
+        return
+    }
+
+    result, err := p.Glob()
+    if err != nil {
+        return
+    }
+
+    files := ext.Files(result.Len())
+    err = result.Into(files)
+    f = files.Files()
+    return
+}
+
